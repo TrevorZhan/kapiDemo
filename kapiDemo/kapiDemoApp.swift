778e95a84f9b76224f550e1c124ad2c7f22c1c24
TrevorZhan <trevorzhan@gmail.com>
2026-03-27 21:44:04 +0800

    Initial Commit

---
+++ kapiDemo/kapiDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  kapiDemoApp.swift
+//  kapiDemo
+//
+//  Created by 詹添 on 3/27/26.
+//
+
+import SwiftUI
+
+@main
+struct kapiDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
